de8581a130f60860556f59f7b8f4ca421a0ee3bb
takanazwa5 <32649717+takanazwa5@users.noreply.github.com>
2025-07-06 08:55:14 +0200

    creddits

--- features/main_menu/main_menu.gd
+++ features/main_menu/main_menu.gd
@@ -1,13 +1,19 @@
 class_name MainMenu extends Control
 
 
+var credits_visible: bool = false
+
+
 @onready var start_button: TextureButton = %StartButton
 @onready var quit_button: TextureButton = %QuitButton
+@onready var credits_button: TextureButton = %CreditsButton
+@onready var credits: TextureRect = %Credits
 
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
+	credits_button.pressed.connect(_on_credits_button_pressed)
 
 
 func _on_start_button_pressed() -> void:
@@ -17,3 +23,12 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	var tween: Tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	if not credits_visible:
+		tween.tween_property(credits, ^"position:x", 187.0, 0.5)
+	else:
+		tween.tween_property(credits, ^"position:x", 320.0, 0.5)
+	credits_visible = not credits_visible
